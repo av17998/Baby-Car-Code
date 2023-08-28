d8678ce9e7321310cbcba55a58a1bfcd45dfffa5
Alex Valdes <atv17998@gmail.com>
2023-08-28 18:01:36 -0400

    Test code is working now

--- main/main.ino
+++ main/main.ino
@@ -2,15 +2,20 @@
 
 #include <Arduino.h>
 #include <HCSR04.h>
-
-HCSR04 hc(2, int[2]{5, 6}, 2); //initialisation class HCSR04 (trig pin , echo pin, number of sensor)
+int testpins[6] = {5, 6, 7, 8, 9, 10};
+HCSR04 hc(2, testpins, 6); //initialisation class HCSR04 (trig pin , echo pin, number of sensor)
 
 void setup()
 { Serial.begin(9600); }
 
 void loop()
 {
-    for (int i = 0; i < 2; i++ )
-        Serial.println( hc.dist(i) ); //return curent distance (cm) in serial for sensor 1 to 6
-    delay(60);                        // we suggest to use over 60ms measurement cycle, in order to prevent trigger signal to the echo signal.
+    for (int i = 0; i < 6; i++ ){
+        Serial.print("Pin");
+        Serial.print(i); 
+        Serial.print(" = ");
+        Serial.print(hc.dist(i));
+        Serial.print("\n"); //return curent distance (cm) in serial for sensor 1 to 6
+        delay(1000);
+        }                       // we suggest to use over 60ms measurement cycle, in order to prevent trigger signal to the echo signal.
 }
